0e8023db26954e90863fe25aa86ef951e47f8969
Gabriel Lins <linsgabriel@hotmail.com>
2019-04-18 10:46:09 -0300

    Update App.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
       resultText: "",
       calculationText: ""
     }
-    this.operations = ['DEL', '+', '-', '*', '/']
+    this.operations = ['DEL', '-', '+', '*', '/']
   }
 
   calculateResult(){
